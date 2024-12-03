e65652d7bd19548fe0546889dc9fc5d33104c9cd
UNI <sinemine1234@gmail.com>
2024-12-03 16:34:38 +0900

    ✨ Delete_task에서 삭제 기능 구현
    tasks.json에 등록된 할 일 중 원하는 기능을 제거하고 할일 목록에서 없앤 것을 제외한 나머지 할 일을 보여주는 기능을 구현

--- todo.py
+++ todo.py
@@ -26,8 +26,13 @@ def complete_task(task_number):#할 일 완료
     pass
 
 def delete_task(task_number):#할 일 삭제
-    pass
-
+    tasks = load_task()
+    if 1<= task_number <= len(tasks): #1 < task_number < 1
+        delete_tsk = tasks.pop(task_number - 1) # index 값 넣어야 한다. /pop() 통해서 삭제 및 반환이 되고 삭제가 된 데이터가 delete_tsk에 들어간다.
+        save_task(tasks)
+        print(f"할 일 : '{delete_tsk['name']}'이(가) 삭제되었습니다.")
+    else:
+        print("유효하지 않은 작업 번호입니다. 다시 확인해주세요.")
 def show_menu(): #메뉴를 보여주는 함수
     print("작업 관리 애플리케이션")
     print("1. 할 일 추가")
